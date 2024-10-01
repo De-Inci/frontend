52300bf174831c7399aefa1020cebd1536deca92
Jeet Mukherjee <jeetmukherjee009@gmail.com>
2024-10-01 14:58:42 +0530

    Added file structure

--- projectu/src/app/page.js
+++ projectu/src/app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Welcome to Incident Management Portal</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h3>Welcome to Incident Management Portal</h3>
     </div>
   );
 }
