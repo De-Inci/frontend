ccd97058162746daa726e79a323a6571c6ff855e
Ranjan00001 <ranjansingh3357@gmail.com>
2024-10-12 15:08:57 +0530

    Brought latest changes from main

---
+++ projectu/src/components/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+
+    return (
+        <div style={{display:'flex', justifyContent:'center'}}>
+            <h1>Welcome to Incident Management Folder</h1>
+
+        </div>
+    )
+}
